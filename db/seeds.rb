ec379ea89c89cb2e863d676e1ea23f7ae983c9da
Leah Perri <perrileah2023@gmail.com>
2023-03-20 14:13:39 -0500

    added model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Pet.create(
+  name: "Juniper",
+  breed: "Australian Shepherd mix",
+  image: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcf-images.us-east-1.prod.boltdns.net%2Fv1%2Fstatic%2F6157254766001%2Fc0cd3b0c-8014-4ee4-8292-79dad099dab8%2F219c6889-47d8-4d4c-8717-3c006e6ba9ab%2F1280x720%2Fmatch%2Fimage.jpg",
+)
+
+Pet.create(
+  name: "Buster",
+  breed: "Blue Heeler",
+  image: "https://www.thesprucepets.com/thmb/fycM3kD4KPoS7arsZA16rwnozbw=/2000x0/filters:no_upscale():strip_icc()/texas-heeler-breed-profile-5194843-hero-4bf5d3fdff4e4cbeaa29d8ff90f9858a.jpeg",
+)
+
+Pet.create(
+  name: "Crush",
+  breed: "Domestic Shorthair",
+  image: "https://images.unsplash.com/photo-1593483316242-efb5420596ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8b3JhbmdlJTIwY2F0fGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+)
